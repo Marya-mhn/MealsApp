4710b20c8922a11130c9bdd4654f266cdbfd20b2
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-05-14 16:44:01 +0100

    commit

--- App.js
+++ App.js
@@ -13,8 +13,20 @@ export default function App() {
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "white" },
+            headerTintColor: "black",
+            contentStyle: { backgroundColor: "white" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
           <Stack.Screen name="MealsOverView" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
