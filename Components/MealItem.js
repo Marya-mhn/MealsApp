e290930a289bdeb3edf8233b7671cf748afe6f36
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-05-14 17:46:32 +0100

    commit

--- Components/MealItem.js
+++ Components/MealItem.js
@@ -7,6 +7,7 @@ import {
   Image,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 function MealItem({
   id,
@@ -36,11 +37,11 @@ function MealItem({
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails
+            duration={duration}
+            affordability={affordability}
+            complexity={complexity}
+          />
         </View>
       </Pressable>
     </View>
@@ -75,17 +76,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     margin: 8,
   },
-  details: {
-    flexDirection: "row",
-    alignItems: "cneter",
-    justifyContent: "center",
-    padding: 8,
-  },
   buttonPressed: {
     opacity: 0.5,
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
   },
 });
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,28 @@
-import { Text } from "react-native";
+import { Text, Image, View } from "react-native";
+
+import { MEALS } from "../data/DummyData";
+
+import MealDetails from "./MealDetails";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
-  return <Text>This is the Meal Detail Screen ({mealId})</Text>;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        affordability={selectedMeal.affordability}
+        complexity={selectedMeal.complexity}
+      />
+      <View></View>
+      <Text>Ingredients</Text>
+
+      <Text>Seps</Text>
+    </View>
+  );
 }
 
 export default MealDetailScreen;
