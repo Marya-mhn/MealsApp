785cd7743e3281bed0d3c6a493028fa8d12903e9
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-05-16 21:47:27 +0100

    Revert "commit"
    This reverts commit 3102298c0c3f5af75b68bbfbeabe16f979295da4.

--- App.js
+++ App.js
@@ -2,22 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Categories" component={CategoriesScreen} />
-    </Drawer.Navigator>
-  );
-}
 
 export default function App() {
   return (
